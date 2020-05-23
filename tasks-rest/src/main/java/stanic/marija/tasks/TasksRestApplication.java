c5bcce7266cfa59f6a1c5fcd2e57113a9b004092
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-23 23:50:58 +0100

    Initial project add

---
+++ tasks-rest/src/main/java/stanic/marija/tasks/TasksRestApplication.java
@@ -0,0 +1,14 @@
+package stanic.marija.tasks;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksRestApplication.class, args);
+	}
+
+}
---
+++ tasks-rest/src/test/java/stanic/marija/tasks/rest/TasksRestApplicationTests.java
@@ -0,0 +1,13 @@
+package stanic.marija.tasks.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
