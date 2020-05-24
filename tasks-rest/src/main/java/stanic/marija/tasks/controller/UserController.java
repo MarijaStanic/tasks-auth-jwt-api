e11fc14513af7e17c7316b46346cb1651a274980
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-24 11:42:53 +0100

    Update user controller

--- tasks-rest/src/main/java/stanic/marija/tasks/controller/UserController.java
+++ tasks-rest/src/main/java/stanic/marija/tasks/controller/UserController.java
@@ -18,27 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import stanic.marija.tasks.dto.UserDataDTO;
 import stanic.marija.tasks.model.User;
+import stanic.marija.tasks.security.JwtTokenRequest;
+import stanic.marija.tasks.security.JwtTokenResponse;
 
 @CrossOrigin()
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-  @Autowired
-  private stanic.marija.tasks.service.UserService userService;
+	@Autowired
+	private stanic.marija.tasks.service.UserService userService;
 
-  @Autowired
-  private ModelMapper modelMapper;
+	@Autowired
+	private ModelMapper modelMapper;
 
-  @PostMapping("/signin")
-  public ResponseEntity<?> login() {
-	 return null;
-  }
+	@PostMapping("/signin")
+	public ResponseEntity<?> login(@RequestBody JwtTokenRequest authenticationRequest) {
+		String token = userService.signin(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		return ResponseEntity.ok(new JwtTokenResponse(token));
+	}
 
-  @PostMapping("/signup")
-  public String signup(@RequestBody UserDataDTO user) {
-    return null;
-  }
-
+	@PostMapping("/signup")
+	public String signup(@RequestBody UserDataDTO user) {
+		return userService.signup(modelMapper.map(user, User.class));
+	}
 
 }
